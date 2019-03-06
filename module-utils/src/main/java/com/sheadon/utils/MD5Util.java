e2e339445f0b2b79315a8fb8d60cf6c2cb8b884e
xudong.sun <289061062@qq.com>
2019-03-06 17:52:39 +0800

    修改

--- module-utils/src/main/java/com/sheadon/utils/MD5Util.java
+++ module-utils/src/main/java/com/sheadon/utils/MD5Util.java
@@ -9,8 +9,9 @@ public class MD5Util {
 
     private static String byteToHexString(byte b) {
         int n = b;
-        if (n < 0)
+        if (n < 0) {
             n += 256;
+        }
         int d1 = n / 16;
         int d2 = n % 16;
         return hexDigits[d1] + hexDigits[d2];
